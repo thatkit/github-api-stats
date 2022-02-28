8cb2cfe9f31241cd79e2f1574cd5d4c176459db2
Nikita <emper137137@gmail.com>
2022-03-01 08:51:54 +1000

    main GET requests are set

--- backend/routes/apiRoute.js
+++ backend/routes/apiRoute.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const fetch = require('cross-fetch');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('GET request to /api');
-    res.status(200).json({ mes: 'successful GET request to /api' });
+const githubApiEndpoint = 'https://api.github.com';
+
+// @ GET        name, location, bio, etc.
+// @ access     PUBLIC
+router.get('/user', (req, res) => {
+    fetch(`${githubApiEndpoint}/users/thatkit`)
+        .then(result => result.text())
+        .then(data => res.status(200).json(JSON.parse(data)))
+        .catch(err => console.log(err)); // # error handler needed
+});
+
+// @ GET        user's repos
+// @ access     PUBLIC
+router.get('/repos', (req, res) => {
+    fetch(`${githubApiEndpoint}/users/thatkit/repos`)
+        .then(result => result.text())
+        .then(data => res.status(200).json(JSON.parse(data)))
+        .catch(err => console.log(err)); // # error handler needed
+});
+
+// @ GET        repo's languages
+// @ access     PUBLIC
+router.get('/langs', (req, res) => {
+    fetch(`${githubApiEndpoint}/repos/thatkit/xpense-tracker/languages`)
+        .then(result => result.text())
+        .then(data => res.status(200).json(JSON.parse(data)))
+        .catch(err => console.log(err)); // # error handler needed
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// body parser
 app.use(express.json());
 
+// routes
 app.use('/api', require('./routes/apiRoute'));
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Listening to port ${port}`));
