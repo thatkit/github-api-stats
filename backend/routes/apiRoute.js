9f54259e5bc5584ec5057ea701185db687ddab79
Nikita <emper137137@gmail.com>
2022-03-06 20:00:46 +1000

    apiRoute refactoring

--- backend/routes/apiRoute.js
+++ backend/routes/apiRoute.js
@@ -3,35 +3,34 @@ const fetch = require('cross-fetch');
 
 const router = express.Router();
 
+// reusable logic blocks
 const githubApiEndpoint = 'https://api.github.com';
-// @ GET        name, location, bio, etc.
-// @ access     PUBLIC
-router.get('/user/:login', (req, res) => {
-    fetch(`${githubApiEndpoint}/users/${req.params.login}`)
+const fetcher = (req, res, modifiedEndpoint) => {
+    fetch(modifiedEndpoint)
         .then(result => result.text())
         .then(data => {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.status(200).json(JSON.parse(data));
         })
         .catch(err => console.log(err)); // # error handler needed
+}
+
+// @ GET        name, location, bio, etc.
+// @ access     PUBLIC
+router.get('/user/:login', (req, res) => {
+    fetcher(req, res, `${githubApiEndpoint}/users/${req.params.login}`);
 });
 
 // @ GET        user's repos
 // @ access     PUBLIC
 router.get('/repos/:login', (req, res) => {
-    fetch(`${githubApiEndpoint}/users/${req.params.login}/repos`)
-        .then(result => result.text())
-        .then(data => res.status(200).json(JSON.parse(data)))
-        .catch(err => console.log(err)); // # error handler needed
+    fetcher(req, res, `${githubApiEndpoint}/users/${req.params.login}/repos`);
 });
 
 // @ GET        repo's languages
 // @ access     PUBLIC
 router.get('/langs/:login/:repo', (req, res) => {
-    fetch(`${githubApiEndpoint}/repos/${req.params.login}/${req.params.repo}/languages`)
-        .then(result => result.text())
-        .then(data => res.status(200).json(JSON.parse(data)))
-        .catch(err => console.log(err)); // # error handler needed
+    fetcher(req, res, `${githubApiEndpoint}/repos/${req.params.login}/${req.params.repo}/languages`);
 });
 
 module.exports = router;
