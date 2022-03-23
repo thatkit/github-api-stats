3068407da8c25dc9d2fa595325f941dcf9cc86ca
Nikita <emper137137@gmail.com>
2022-03-23 21:12:43 +1000

    mockup & apiService are connected & need to refactor and renovate tooltipEl

--- client/src/components/PieChart/externalTooltip.js
+++ client/src/components/PieChart/externalTooltip.js
@@ -1,19 +1,10 @@
-const getOrCreateTooltip = (chart) => {
+const getOrCreateTooltip = chart => {
   let tooltipEl = chart.canvas.parentNode.querySelector('div');
 
   if (!tooltipEl) {
-
     // create CONTAINER div
     tooltipEl = document.createElement('div');
-
-    tooltipEl.style.background = 'rgba(0, 0, 0, 0.7)';
-    tooltipEl.style.borderRadius = '4px';
-    tooltipEl.style.color = 'white';
-    tooltipEl.style.opacity = 1;
-    tooltipEl.style.pointerEvents = 'none';
-    tooltipEl.style.position = 'absolute';
-    tooltipEl.style.transform = 'translate(-50%, 0)';
-    tooltipEl.style.transition = 'all .1s ease';
+    tooltipEl.className = 'tooltipEl';
   }
 
   return tooltipEl;
@@ -23,7 +14,7 @@ export const externalTooltipHandler = (context) => {
   // Tooltip Element
   const {chart, tooltip} = context;
 
-  // console.log(chart)
+  console.log(chart)
   console.log(tooltip)
 
   const tooltipEl = getOrCreateTooltip(chart);
@@ -41,9 +32,11 @@ export const externalTooltipHandler = (context) => {
       tooltipEl.firstChild.remove();
     }
 
+    const selectedLang = tooltip.dataPoints[0].label;
+
     // create HEADING for the language name 
     const h3 = document.createElement('h3');
-    h3.innerText = tooltip.dataPoints[0].label;
+    h3.innerText = selectedLang;
     tooltipEl.appendChild(h3);
 
     // create SPAN for the number of lines
@@ -57,20 +50,20 @@ export const externalTooltipHandler = (context) => {
 
     // console.log(tooltip.dataPoints[0].dataset.repos)
     tooltip.dataPoints[0].dataset.repos
-      .filter(repo => Object.keys(repo.langs).includes(tooltip.dataPoints[0].label))
-      .forEach(repo => {
+      .filter(repo => Object.keys(repo.langs).includes(selectedLang))
+      .sort((a, b) => b.langs[selectedLang] - a.langs[selectedLang])
+      .forEach(({ githubUrl, name }) => {
         const li = document.createElement('li');
         
-        li.innerHTML = `
-          <a href='${repo.url}' target='_blank'>${repo.name}</a>
-        `;
+        li.innerHTML = `<a href='${githubUrl}' target='_blank'>${name}</a>`;
         
         ul.appendChild(li);
       });
+      
     tooltipEl.appendChild(ul);
   }
 
-  const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
+  const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
 
   // Display, position, and set styles for font
   tooltipEl.style.opacity = 1;
