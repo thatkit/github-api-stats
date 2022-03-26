a13e125e5257843d5f1c9a838fe4635c91ad5e72
Nikita <emper137137@gmail.com>
2022-03-26 18:55:45 +1000

    github api rate limit 15 000 reached successfully

--- mockupBackend/routes/apiRoute.js
+++ mockupBackend/routes/apiRoute.js
@@ -1,18 +1,35 @@
 const express = require('express');
-const { Octokit } = require('@octokit/rest');
+const { Octokit } = require('@octokit/core');
+const { request } = require('@octokit/request');
+const { createAppAuth  } = require('@octokit/auth-app');
 require('dotenv').config({ path: './mockupBackend/config/.env' });
 const selectRepos = require('../middleware/selectRepos');
 const updateReposWithLangs = require('../middleware/updateReposWithLangs');
 const sumLangs = require('../middleware/sumLangs');
 
 const router = express.Router();
-const octokit = new Octokit({ auth: process.env.AUTH_TOKEN });
+
+const auth = createAppAuth({
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    installationId: 24432550
+});
+let token = '';
+auth({ type: 'installation' }).then(data => {
+    token = data.token;
+})
+const octokit = new Octokit();
 
 // @ GET        name, location, bio, etc.
 // @ access     PUBLIC
 router.get('/user/:login', (req, res) => {
-    octokit
-        .request(`GET /users/${req.params.login}`)
+    octokit.request(`GET /users/${req.params.login}`, {
+            headers: { authorization: `token ${token}` },
+            org: 'thatkit-org',
+            type: 'installation'
+        })
         .then(user => res.status(200).json(user))
         .catch(err => console.log(err)); // # error handler needed
 });
