9ade430b8c787050b10c2393d16a71e44b7af09c
Nikita <emper137137@gmail.com>
2022-03-27 10:14:12 +1000

    githubAuth middleware module

--- backend/routes/apiRoute.js
+++ backend/routes/apiRoute.js
@@ -1,38 +1,19 @@
 const express = require('express');
-let { request } = require('@octokit/request');
-const { createAppAuth  } = require('@octokit/auth-app');
 require('dotenv').config({ path: './backend/config/.env' });
 // Middleware
+const githubAuth = require('../middleware/githubAuth');
 const selectRepos = require('../middleware/selectRepos');
 const updateReposWithLangs = require('../middleware/updateReposWithLangs');
 const sumLangs = require('../middleware/sumLangs');
 
 const router = express.Router();
-
-// Auth middleware config
-const auth = createAppAuth({
-    appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    installationId: process.env.INSTALLATION_ID
-});
-
-router.use((req, res, next) => {
-    auth({ type: 'installation' }).then(({ token }) => {
-        request = request.defaults({
-            headers: { authorization: `token ${token}` },
-            org: 'thatkit-org',
-            type: 'installation'
-        });
-        next();
-    });
-});
+// Auth middleware
+router.use(githubAuth);
 
 // @ GET        user's info
 // @ access     PUBLIC
 router.get('/user/:login', (req, res) => {
-    request(`GET /users/${req.params.login}`)
+    req.requestWithAuth(`GET /users/${req.params.login}`)
         .then(user => res.status(200).json(user))
         .catch(err => console.log(err)); // # error handler needed
 });
