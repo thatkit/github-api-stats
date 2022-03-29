d09b06612fcbb048c17ccb099a3d0a1dbe4149c7
Nikita <emper137137@gmail.com>
2022-03-29 20:39:45 +1000

    probably need to rewrite redux

--- client/src/redux/apiService.js
+++ client/src/redux/apiService.js
@@ -1,10 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { getCookie } from '../helpers/cookies';
+import { getCookie, setCookie } from '../helpers/cookies';
 
 export const apiService = createApi({
     reducerPath: 'api',
-    tagTypes: ['cacheee'],
-    keepUnusedDataFor: 30,
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:5000/api',
         prepareHeaders: (headers, { getState }) => {
@@ -16,39 +14,7 @@ export const apiService = createApi({
     }),
     endpoints: build => ({
         getAuthToken: build.query({
-            query: () => '/auth',
-            providesTags: (result, error) => [{ type: 'cacheee' }],
-            // The 2nd parameter is the destructured `QueryLifecycleApi`
-            async onQueryStarted(
-                arg,
-                {
-                    dispatch,
-                    getState,
-                    extra,
-                    requestId,
-                    queryFulfilled,
-                    getCacheEntry,
-                    updateCachedData,
-                }
-            ) {
-                console.log(await queryFulfilled)
-            },
-            // The 2nd parameter is the destructured `QueryCacheLifecycleApi`
-            async onCacheEntryAdded(
-                arg,
-                {
-                    dispatch,
-                    getState,
-                    extra,
-                    requestId,
-                    cacheEntryRemoved,
-                    cacheDataLoaded,
-                    getCacheEntry,
-                    updateCachedData,
-                }
-            ) {
-                console.log(await cacheEntryRemoved)
-            },
+            query: () => '/auth'
         }),
         getUserByLogin: build.query({
             query: login => `/user/${login}`
@@ -64,9 +30,3 @@ export const {
     useGetUserByLoginQuery,
     useGetLangsByLoginQuery
 } = apiService;
-
-/*
-
-Now I need to figure out how those onQueryEntry etc. func work in order to maintain a clean logic for caching token and using as a header
-
-*/
