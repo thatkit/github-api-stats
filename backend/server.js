280b53335b443ea268f64ffdbc5ee9d4d7f263a8
Nikita <emper137137@gmail.com>
2022-04-17 14:24:05 +1000

    ready for Glitch deploy

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,12 @@ app.use('/', (req, res, next) => {
 // routes
 app.use('/api', require('./routes/apiRoute'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
