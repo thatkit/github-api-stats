a8bd5ffae525473aab5044c76e61ba18455d858a
Nikita <emper137137@gmail.com>
2022-04-17 10:08:59 +1000

    slight css refactor

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,14 +3,12 @@ import styles from './Footer.module.css';
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>Made by</p>
-            &nbsp;
             <a
                 className={styles.link}
                 href="https://github.com/thatkit"
                 target="_blank"
                 rel="noreferrer"
-            >thatkit</a>
+            >Github</a>
         </footer>
     )
 }
